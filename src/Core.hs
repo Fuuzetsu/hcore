ef4cdf2e6d98912c7ba3c7bfd1fbd54f2e350f97
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-11-27 17:38:19 +0000

    1.3

--- src/Core.hs
+++ src/Core.hs
@@ -1,1 +1,41 @@
 module Core where
+
+-- | Miranda converts between big-num integers
+-- and floats automatically. We simply default to 'Double'
+-- until it starts causing us problems, if ever.
+type Number = Double
+
+data Expr a = EVar Name
+            | ENum Number
+            | EConstr Number Number
+            | EAp (Expr a) (Expr a)
+            | ELet IsRec [(a, Expr a)] (Expr a)
+            | ECase (Expr a) [Alter a]
+            | ELam [a] (Expr a)
+
+type CoreExpr = Expr Name
+type Name = String
+type IsRec = Bool
+
+recursive = True
+nonRecursive = False
+
+bindersOf :: [(a, b)] -> [a]
+bindersOf defns = [name | (name, rhs) <- defns]
+
+rhssOf :: [(a, b)] -> [b]
+rhssOf defns = [rhs | (name, rhs) <- defns]
+
+type Alter a = (Number, [a], Expr a)
+type CoreAlt = Alter Name
+
+isAtomicExpr :: Expr a -> Bool
+isAtomicExpr (EVar v) = True
+isAtomicExpr (ENum n) = True
+isAtomicExpr e        = False
+
+type Program a = [ScDefn a]
+type CoreProgram = Program String
+
+type ScDefn a = (Name, [a], Expr a)
+type CoreScDefn = ScDefn String
